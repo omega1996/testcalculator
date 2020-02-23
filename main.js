e58b8bcf8b5346f8ce5d8b918ebc855b1ffc4859
IvanR <yu181219710>
2020-02-23 17:08:05 +0500

    code style fixes

--- main.js
+++ main.js
@@ -1,71 +1,64 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const int = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-int.question('Введите выражение: ', (answer) => {
-    // TODO: complete the task
+int.question("Введите выражение: ", (answer) => {
     parseAnswer(answer)
         .then(calculateTree)
-        .then(calculated=>console.log(`Результат: ${calculated}`))
-        .catch(error=>{console.error(error)})
-        .then(int.close())
+        .then(calculated => console.log(`Результат: ${calculated}`))
+        .catch(error => {console.error(error)})
+        .then(int.close());
 
 });
 
-const sum = (...args) => args.reduce((prev,curr)=>prev+curr)  //subtraction is a type of summation
-const div = (a, b) => a/b
-const mul = (a, b) => a * b
+const sum = (...args) => args.reduce((prev,curr)=>prev+curr);  //subtraction is a type of summation
+const div = (a, b) => a/b;
+const mul = (a, b) => a * b;
 
-const parseAnswer = (answer)=>{
-    let promise = new Promise(resolve=>{
-        let matchedArray = answer.match(/([)(*/]|[-+]?\d[.]?\d*|[-])/g)
-        matchedArray = matchedArray.map(element => !isNaN(parseFloat(element)) ?  parseFloat(element) : element)
-        resolve(matchedArray)
+const parseAnswer = (answer) => {
+    return new Promise(resolve => {
+        let matchedArray = answer.match(/([)(*/]|[\-+]?\d[.]?\d*|[\-])/g);
+        matchedArray = matchedArray.map(element => !isNaN(parseFloat(element)) ? parseFloat(element) : element);
+        resolve(matchedArray);
     });
-    return promise
-}
+};
 
-const evaluateMath = (mathExp)=>{
-    if (Array.isArray(mathExp)){
-        if (mathExp.indexOf('-')!== -1){
-            let subIndex = mathExp.indexOf('-')
-            mathExp.splice(subIndex, 2, mathExp[subIndex+1]*(-1))
-            return evaluateMath(mathExp)
-        } else if (mathExp.lastIndexOf('*') !== -1){
-            let multiplyIndex = mathExp.lastIndexOf('*')
-            let resultMul = mul(mathExp[multiplyIndex-1],mathExp[multiplyIndex+1])
-            mathExp.splice(multiplyIndex-1, 3, resultMul)
-            return evaluateMath(mathExp)
-        } else if (mathExp.lastIndexOf('/') !== -1){
-            let multiplyIndex = mathExp.lastIndexOf('/')
-            let resultMul = div(mathExp[multiplyIndex-1], mathExp[multiplyIndex+1])
-            mathExp.splice(multiplyIndex-1, 3, resultMul)
-            return evaluateMath(mathExp)
-        }
-        else {
-            return sum(...mathExp)
-        }
-    } else {
-        return mathExp
+const evaluateMath = (mathExp) => {
+    if (!Array.isArray(mathExp)) return mathExp;
+
+    if (mathExp.indexOf("-") !== -1){
+        let subIndex = mathExp.indexOf("-");
+        mathExp.splice(subIndex, 2, mathExp[subIndex + 1] * (-1));
+        return evaluateMath(mathExp);
+    } else if (mathExp.lastIndexOf("*") !== -1){
+        let multiplyIndex = mathExp.lastIndexOf("*");
+        let resultMul = mul(mathExp[multiplyIndex - 1], mathExp[multiplyIndex + 1]);
+        mathExp.splice(multiplyIndex - 1, 3, resultMul);
+        return evaluateMath(mathExp);
+    } else if (mathExp.lastIndexOf("/") !== -1){
+        let multiplyIndex = mathExp.lastIndexOf("/");
+        let resultMul = div(mathExp[multiplyIndex - 1], mathExp[multiplyIndex + 1]);
+        mathExp.splice(multiplyIndex - 1, 3, resultMul);
+        return evaluateMath(mathExp);
     }
-}
+    else {
+        return sum(...mathExp);
+    }
+
+};
 
 const calculateTree = (fwdList)=>{
-    if(fwdList.indexOf(')') !== -1){
-        let LeftSide = fwdList.slice(0,fwdList.indexOf(')'))
-        let cutList = LeftSide.slice(fwdList.lastIndexOf('(')+1)
-        let cutListLength = cutList.length+2
-        let mathRes = evaluateMath(cutList)
-        fwdList.splice(LeftSide.lastIndexOf('('), cutListLength, mathRes)
-        let answer = calculateTree(fwdList)
-        return answer
-    } else {
-        let answer = evaluateMath(fwdList)
-        return answer
-    }
-}
+    if(fwdList.indexOf(")") === -1) return evaluateMath(fwdList);
 
-module.exports = {sum, div, mul, calculateTree, evaluateMath, parseAnswer}
+    let LeftSide = fwdList.slice(0,fwdList.indexOf(")"));
+    let cutList = LeftSide.slice(fwdList.lastIndexOf("(") + 1);
+    let cutListLength = cutList.length + 2;
+    let mathRes = evaluateMath(cutList);
+    fwdList.splice(LeftSide.lastIndexOf("("), cutListLength, mathRes);
+    return calculateTree(fwdList);
+};
+
+module.exports = {sum, div, mul, calculateTree, evaluateMath, parseAnswer};
--- main.test.js
+++ main.test.js
@@ -1,26 +1,26 @@
-const {sum, mul, div, calculateTree, evaluateMath, parseAnswer} = require('./main');
+const {sum, mul, div, calculateTree, evaluateMath, parseAnswer} = require("./main");
 
-describe('Math functions:', ()=>{
+describe("Math functions:", () => {
 
-    test('Sum should return sum of one or more values', ()=>{
-        expect(sum(1,3)).toBe(4);
+    test("Sum should return sum of one or more values", () => {
+        expect(sum(1, 3)).toBe(4);
         expect(sum(1)).toBe(1);
-        expect(sum(1.5,3.5)).toBe(5);
-        expect(sum(0.5,-3.5)).toBe(-3);
-        expect(sum(0.5,-3.5, 2.3)).toBeCloseTo(-0.7);
-        expect(sum(0.5,3.5, 2.3)).toBeCloseTo(6.3);
-        expect(sum(...[0.5,3.5, 2.3])).toBeCloseTo(6.3);
+        expect(sum(1.5, 3.5)).toBe(5);
+        expect(sum(0.5, -3.5)).toBe(-3);
+        expect(sum(0.5, -3.5, 2.3)).toBeCloseTo(-0.7);
+        expect(sum(0.5, 3.5, 2.3)).toBeCloseTo(6.3);
+        expect(sum(...[0.5, 3.5, 2.3])).toBeCloseTo(6.3);
         expect(sum(...[1])).toBe(1);
     });
 
-    test('Mul should return multiplication of two values',()=>{
+    test("Mul should return multiplication of two values",() => {
         expect(mul(1,3)).toBe(3);
         expect(mul(1,0.3)).toBe(0.3);
         expect(mul(1,0)).toBe(0);
         expect(mul(0.5,2)).toBe(1);
     });
 
-    test('Div should return quotient of two values',()=>{
+    test("Div should return quotient of two values",() => {
         expect(div(6,3)).toBe(2);
         expect(div(10,3)).toBeCloseTo(3.333);
         expect(div(1,0)).toBeCloseTo(Infinity);
@@ -28,66 +28,66 @@ describe('Math functions:', ()=>{
 });
 
 
-describe('Math evaluation functions:',()=>{
+describe("Math evaluation functions:",() => {
 
-    test('Should return a number',()=>{
+    test("Should return a number",() => {
         expect(evaluateMath(3)).not.toBeNaN();
-        expect(typeof evaluateMath([3])).toBe('number');
-        expect(typeof evaluateMath([3, 3])).toBe('number');
-        expect(typeof evaluateMath([3, '/', 3])).toBe('number');
+        expect(typeof evaluateMath([3])).toBe("number");
+        expect(typeof evaluateMath([3, 3])).toBe("number");
+        expect(typeof evaluateMath([3, "/", 3])).toBe("number");
     });
 
-    test('Should return a valid math result',()=>{
+    test("Should return a valid math result",() => {
         expect(evaluateMath(3)).toBe(3);
         expect(evaluateMath([3])).toBe(3);
         expect(evaluateMath([3, 3])).toBe(6);
-        expect(evaluateMath([3, '/', 3])).toBe(1);
-        expect(evaluateMath([3, '*', 3])).toBe(9);
-        expect(evaluateMath([4, '*', 0.5])).toBe(2);
-        expect(evaluateMath([2, 2, '*', 2])).toBe(6);
-        expect(evaluateMath([2, -2, '*', 2])).toBe(-2);
+        expect(evaluateMath([3, "/", 3])).toBe(1);
+        expect(evaluateMath([3, "*", 3])).toBe(9);
+        expect(evaluateMath([4, "*", 0.5])).toBe(2);
+        expect(evaluateMath([2, 2, "*", 2])).toBe(6);
+        expect(evaluateMath([2, -2, "*", 2])).toBe(-2);
         expect(evaluateMath([1, -1, 1, -1])).toBe(0);
         expect(evaluateMath([1, 2, -3])).toBe(0);
-        expect(evaluateMath([3, '/', -3])).toBe(-1);
-        expect(evaluateMath([1, '/',  -2])).toBe(-0.5);
+        expect(evaluateMath([3, "/", -3])).toBe(-1);
+        expect(evaluateMath([1, "/",  -2])).toBe(-0.5);
     });
 });
 
-describe('Parsing functions:',()=>{
+describe("Parsing functions:",() => {
 
-    test('Should return an Array',async ()=> {
-        expect( Array.isArray( await parseAnswer('2*(2-2)'))).toBeTruthy()
+    test("Should return an Array",async () => {
+        expect( Array.isArray( await parseAnswer("2*(2-2)"))).toBeTruthy()
     });
 
-    test('Should correctly work with numbers',async ()=>{
-        expect( await parseAnswer('2+2')).toEqual([2,2]);
-        expect( await parseAnswer('2-2')).toEqual([2,-2]);
+    test("Should correctly work with numbers",async ()=>{
+        expect( await parseAnswer("2+2")).toEqual([2,2]);
+        expect( await parseAnswer("2-2")).toEqual([2,-2]);
     });
 
-    test('Should correctly work with brackets and math signs',async ()=> {
-        expect( await parseAnswer('2-(2-2)')).toEqual([2,'-','(',2,-2,')']);
-        expect( await parseAnswer('2*(2-2)')).toEqual([2,'*','(',2,-2,')']);
-        expect( await parseAnswer('2*(1+(2-2))')).toEqual([2,'*','(',1,'(',2,-2,')',')']);
+    test("Should correctly work with brackets and math signs",async ()=> {
+        expect( await parseAnswer("2-(2-2)")).toEqual([2,"-","(",2,-2,")"]);
+        expect( await parseAnswer("2*(2-2)")).toEqual([2,"*","(",2,-2,")"]);
+        expect( await parseAnswer("2*(1+(2-2))")).toEqual([2,"*","(",1,"(",2,-2,")",")"]);
     });
-    test('Should not return "+" signs',async ()=> {
-        expect( await parseAnswer('2+(2-2)')).toEqual([2,'(',2,-2,')']);
+    test("Should not return '+' signs",async ()=> {
+        expect( await parseAnswer("2+(2-2)")).toEqual([2,"(",2,-2,")"]);
     });
 });
 
 
-describe('Calculating functions:',()=>{
+describe("Calculating functions:",()=>{
 
-    test('Should calculate simple expressions',async ()=> {
+    test("Should calculate simple expressions",async ()=> {
         expect( calculateTree([1,1])).toBe(2);
         expect( calculateTree([1,-1])).toBe(0);
         expect( calculateTree([1,2,-3])).toBe(0);
     });
 
-    test('Should calculate expressions with brackets',async ()=> {
+    test("Should calculate expressions with brackets",async ()=> {
         expect( calculateTree([1,1])).toBe(2);
-        expect( calculateTree([2,'*','(',1,'(',2,-2,')',')'])).toBe(2);
-        expect( calculateTree([2,'-','(',2,-2,')'])).toBe(2);
-        expect( calculateTree([2,'(',2,-2,')'])).toBe(2);
-        expect( calculateTree([2,'(',2,'*',-2,')'])).toBe(-2);
+        expect( calculateTree([2,"*","(",1,"(",2,-2,")",")"])).toBe(2);
+        expect( calculateTree([2,"-","(",2,-2,")"])).toBe(2);
+        expect( calculateTree([2,"(",2,-2,")"])).toBe(2);
+        expect( calculateTree([2,"(",2,"*",-2,")"])).toBe(-2);
     });
 });
